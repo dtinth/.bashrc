ee5f5bb7da9df8eb8b7a35ffa6d5e19c777ed33f
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2014-06-04 09:51:01 +0700

    initial commit

---
+++ alias_git.sh
@@ -0,0 +1,8 @@
+
+alias A='git add -A'
+alias S='git status'
+alias SU='git status -uall'
+alias C='git commit'
+alias D='git diff'
+alias DH='git diff HEAD'
+
---
+++ alias_reshell.sh
@@ -0,0 +1,2 @@
+
+alias reshell='exec bash -l'
