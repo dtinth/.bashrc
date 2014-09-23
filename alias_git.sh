63139bdbec5bdb849d4efe462b57c59e2d5823e0
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2014-09-23 11:57:15 +0700

    more git aliases, using tricks learnt from Code School

--- alias_git.sh
+++ alias_git.sh
@@ -1,7 +1,11 @@
 
+alias git=hub
 alias A='git add -A'
-alias S='git status'
-alias SU='git status -uall'
+alias S='git status -s'
+alias L='git log --oneline --decorate --all --graph'
+alias SL='git status'
+alias SU='git status -uall -s'
+alias SUL='git status -uall'
 alias C='git commit'
 alias D='git diff'
 alias DH='git diff HEAD'
--- alias_vim.sh
+++ alias_vim.sh
@@ -1,3 +1,3 @@
 
 # http://superuser.com/a/393363/333845
-vimx() { vim "$@" && chmod +x "$@"; };
+vx() { vim "$@" && chmod +x "$@"; };
